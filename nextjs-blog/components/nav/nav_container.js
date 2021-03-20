6df4439663496941c9c1f2c08ab547310fbc1a2f
Lucas <lucas.larnicol@gmail.com>
2021-03-20 15:40:00 +0100

    NavBar first commit

--- nextjs-blog/components/nav/nav_container.js
+++ nextjs-blog/components/nav/nav_container.js
@@ -1,7 +1,29 @@
 import styles from './nav_container.module.css'
+import Col from '../utils/col.js'
+import Row from '../utils/row.js'
 
 export default function nav_container(props){
     return <>
-
+        <nav>
+            <Row>
+                <Col size="8">
+                    <figure>  
+                        LOGO ICI
+                    </figure>
+                </Col>
+                <Col size="4">
+                    <section>
+                        <Row>
+                            <Col size="4">
+                                <a>About</a>
+                            </Col>
+                            <Col size="4">
+                                <a>Contact</a>
+                            </Col>
+                        </Row>
+                    </section>
+                </Col>
+            </Row>
+        </nav>
     </>
 }
