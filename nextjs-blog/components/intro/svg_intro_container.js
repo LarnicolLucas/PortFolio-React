46d40335c2801bd4d3d0ce89721632d84e4e9bf1
Lucas <lucas.larnicol@gmail.com>
2021-03-28 09:37:43 +0100

    add Name in logo

--- nextjs-blog/components/intro/svg_intro_container.js
+++ nextjs-blog/components/intro/svg_intro_container.js
@@ -19,9 +19,11 @@ export default function containe_SVG() {
     return(
         <section onMouseMove={givePos} className={styles.container}>
             <figure className={styles.animeLogo}>
+                <SVGIntro src="/images/la.svg" posX={-2*posX}></SVGIntro>
                 <SVGIntro src="/images/left.svg" posX={posX} posY={posY}></SVGIntro>
                 <SVGIntro src="/images/right.svg" posX={-posX} posY={-posY}></SVGIntro>
                 <SVGIntro src="/images/slash.svg" posX={(-1.5) * posX} posY={1.7 * posY}></SVGIntro>
+                <SVGIntro src="/images/lu.svg" posX={0.8* posX}></SVGIntro>
             </figure>
         </section>
     )
