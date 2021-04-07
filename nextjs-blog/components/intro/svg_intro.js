83cb98e2e36e20df7f4873d24dd92a2c50f29503
Lucas <lucas.larnicol@gmail.com>
2021-04-07 19:59:00 +0200

    logic work component container pres

--- nextjs-blog/components/intro/svg_intro.js
+++ nextjs-blog/components/intro/svg_intro.js
@@ -1,9 +1,33 @@
 import styles from './svg_intro.module.css'
+import  {gsap} from 'gsap'
+import {useRef, useEffect} from 'react'
 
 export default function SVGIntro(props){
+
+    const img_dom_element = useRef();
+    const randomCo = ()=> Math.ceil(Math.random() * 100);
+
+    const anim_params_in = {x: 0, y: 0, duration: 1};
+    const anim_params_out = {x: randomCo(), y: randomCo() , duration: 1, ease: "elastic.out(1, 0.3)"}
+
+    const anime = (element, anim_params)=> gsap.to(element, anim_params);
+
+    useEffect(()=> {
+        if(props.anime === true){
+            anime(img_dom_element.current, anim_params_in)
+        } else if(props.anime === false){
+            anime(img_dom_element.current, anim_params_out)
+        }
+    })
+
     return <>
 
-        <img src={props.src} className = {styles.svg} style={{left: props.posX+"px", top: props.posY+"px"}}/>
+        <img 
+            ref={img_dom_element}
+            src={props.src} 
+            className = {styles.svg} 
+            style={{left: props.posX+"px", top: props.posY+"px"}}
+        />
 
     </>
 }
--- nextjs-blog/components/pres/pres.js
+++ nextjs-blog/components/pres/pres.js
@@ -1,10 +1,8 @@
 import {useState, useEffect, useRef} from 'react'
 import styles from './pres.module.sass'
-import { gsap } from 'gsap'
 import Svg from '../intro/svg_intro.js'
 
 export default function pres(props){
-
     const container = useRef();
 
     const options_scroll_observer = {
@@ -13,49 +11,55 @@ export default function pres(props){
         rootMargin: "0px"
     };
 
-    const randomGSAP = gsap.utils.random(-200, 200, 5, true);
-
-    const animeIn = (list_elements)=> list_elements.forEach(el => gsap.to(el.current, {x: 100, duration: 1, ease: "elastic.out(1, 0.3)"}) );
-    const animeOut = (list_elements)=> list_elements.forEach(el => gsap.to(el.current, {x: 0, duration: 1}));
-
     const createListOfSvg = (number_of_svg) => {
 
         const addRouteNameList = (list, number) => {
             const svg_name_type = "face";
             const route = "/images/face/";
-            return list.concat({route :`${route}${svg_name_type + number}.svg`, id: number})
+            return list.concat({
+                route :`${route}${svg_name_type + number}.svg`, 
+                id: number
+            })
         };
 
         const recursiveFunction = (nb, iteration, res, fn) => iteration > nb ? res : recursiveFunction(nb, iteration + 1, fn(res, iteration), fn);
 
         return recursiveFunction(number_of_svg, 0, [], addRouteNameList)
-    }
-
+    };
     const svg_rout_list = createListOfSvg(11);
-    const svg_dom_ref = svg_rout_list.map(el => useRef())
-    const svg_childs = svg_rout_list.map(el => <Svg ref={svg_dom_ref[el.id]} key={el.id} src={el.route} />);
 
+    const createChildList = (animation)=> svg_rout_list.map(el => <Svg 
+        key={el.id} 
+        src={el.route} 
+        anime={animation}
+    />);
+
+    const [svg_childs, setSvgChilds] = useState(createChildList(false));
+
+    const animeIn = () => setSvgChilds(createChildList(true));
+    const animeOut = () => setSvgChilds(createChildList(false));
 
     const intersectionFunction = (entries, observer) => {
         //no Functional
         entries.forEach(el => {
-            if(el.intersectionRatio > options_scroll_observer.threshold) return animeIn(svg_dom_ref)
-            return animeOut(svg_dom_ref)
-            
+            if(el.intersectionRatio > options_scroll_observer.threshold) return animeIn()
+            return animeOut()
         })
-    }
+    };
+
+    const observer = useRef(null)
 
     useEffect( ()=> {
-        const observer = new IntersectionObserver(intersectionFunction, options_scroll_observer);
-        observer.observe(container.current)
-
-    });
+        if (observer.current) observer.current.disconnect() ;
+        observer.current = new IntersectionObserver(intersectionFunction, options_scroll_observer);
+        observer.current.observe(container.current);
+        return function cleanObserver(){ observer.current.disconnect() }
+    }, []);
     
 
     return <>
         <div ref={container} className={styles.container}>
             {svg_childs}
-            {console.log(svg_childs)}
         </div>
     </>
 }
