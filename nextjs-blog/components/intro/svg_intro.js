56e4d7e68f9535119db9bcf9497a22489b010083
Lucas <lucas.larnicol@gmail.com>
2021-04-07 21:17:17 +0200

    elastic effect in reconstitution face

--- nextjs-blog/components/intro/svg_intro.js
+++ nextjs-blog/components/intro/svg_intro.js
@@ -7,8 +7,8 @@ export default function SVGIntro(props){
     const img_dom_element = useRef();
     const randomCo = ()=> Math.ceil(Math.random() * 100);
 
-    const anim_params_in = {x: 0, y: 0, duration: 1};
-    const anim_params_out = {x: randomCo(), y: randomCo() , duration: 1, ease: "elastic.out(1, 0.3)"}
+    const anim_params_in = {x: 0, y: 0, duration: 1, ease: "elastic.out(1, 0.3)"};
+    const anim_params_out = {x: randomCo(), y: randomCo() , duration: 1}
 
     const anime = (element, anim_params)=> gsap.to(element, anim_params);
 
