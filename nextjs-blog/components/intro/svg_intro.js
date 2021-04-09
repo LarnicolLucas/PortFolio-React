1a2ad391d3c4d3ce34b93574a666665a3ba44da2
Lucas <lucas.larnicol@gmail.com>
2021-04-09 07:41:06 +0200

    animation SVG is in props now

--- nextjs-blog/components/intro/svg_intro.js
+++ nextjs-blog/components/intro/svg_intro.js
@@ -5,20 +5,11 @@ import {useRef, useEffect} from 'react'
 export default function SVGIntro(props){
 
     const img_dom_element = useRef();
-    const randomCo = (min, max)=> Math.ceil((Math.random() * (max - min)) + min);
-
-    const anim_params_in = {x: 0, y: 0, duration: 1.5, ease: "elastic.out(1, 0.3)"};
-    const anim_params_out = {x: randomCo(700, -200), y: randomCo(500, 0) , duration: 1}
-
-    const anime = (element, anim_params)=> gsap.to(element, anim_params);
 
     useEffect(()=> {
-        if(props.anime === true){
-            anime(img_dom_element.current, anim_params_in)
-        } else if(props.anime === false){
-            anime(img_dom_element.current, anim_params_out)
-        }
-    })
+        if(props.anime === undefined) return ;
+        props.animation(img_dom_element.current, props.anime)
+    }, [props.anime])
 
     return <>
 
