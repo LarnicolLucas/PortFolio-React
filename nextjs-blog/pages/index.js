06e9a18b47d223f3eff848bd9595b0ff28e48064
Lucas <lucas.larnicol@gmail.com>
2021-04-11 20:01:59 +0200

    add section in index for page height

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -2,14 +2,17 @@ import Layout from '../components/layout'
 import NAV_container from '../components/nav/nav_container.js'
 import SVG_intro_container from '../components/intro/svg_intro_container.js'
 import Pres from '../components/pres/pres.js'
+import styles from './index.module.sass'
 
 export default function index(props){
 
   return <>
       <Layout>
-          <NAV_container></NAV_container>
-          <SVG_intro_container></SVG_intro_container>
-          <Pres></Pres>
+          <section className={styles.page}>
+            <NAV_container/>
+            <SVG_intro_container/>
+          </section>
+          <Pres/>
       </Layout>
     </>
 }
