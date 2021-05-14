682757020698a578e2642c049d8cd44500da9576
Lucas <lucas.larnicol@gmail.com>
2021-05-14 17:13:59 +0200

    add some skills

--- nextjs-blog/components/skills/skills_list.js
+++ nextjs-blog/components/skills/skills_list.js
@@ -1,15 +1,23 @@
 export default  {
     front: [
         {id: 0, name: "React", src: "/images/icons_devs/react.svg"},
-        {id: 1, name: "VueJs", src: "/images/icons_devs/vuejs.svg"}
+        {id: 1, name: "Vue", src: "/images/icons_devs/vuejs.svg"},
+        {id: 2, name: "CSS", src: "/images/icons_devs/css.svg"},
+        {id: 3, name: "HTML", src: "/images/icons_devs/html.svg"},
+        {id: 4, name: "JSON", src: "/images/icons_devs/json.svg"},
+        {id: 5, name: "JWT", src: "/images/icons_devs/jwt.svg"},
     ],
     back: [
         {id: 0, name: "NodeJS", src: "/images/icons_devs/nodejs.svg"},
-        {id: 1, name: "SQL Server", src: "/images/icons_devs/sqlserver.svg"}
+        {id: 1, name: "SQL Server", src: "/images/icons_devs/sqlserver.svg"},
+        {id: 2, name: "MongoDB", src: "/images/icons_devs/mongodb.svg"},
     ],
     design: [
         {id: 0, name: "Inskape", src: "/images/icons_devs/inskape.svg"},
-        {id: 1, name: "Gimp", src: "/images/icons_devs/gimp.svg"}
+        {id: 1, name: "Gimp", src: "/images/icons_devs/gimp.svg"},
+        {id: 2, name: "Photoshop", src: "/images/icons_devs/photoshop.svg"},
+        {id: 3, name: "Illustrator", src: "/images/icons_devs/illustrator.svg"},
+        {id: 4, name: "Premiere", src: "/images/icons_devs/premiere.svg"},
     ],
 
 }
