3097be04b6aca1192d74abc3022603f8c029f5ca
Lucas <lucas.larnicol@gmail.com>
2021-05-15 19:45:54 +0200

    add mouse effect on list skills

--- nextjs-blog/components/skills/list/list.js
+++ nextjs-blog/components/skills/list/list.js
@@ -1,10 +1,16 @@
+import {useState} from 'react'
 import styles from './list.module.sass'
 
 
 export default function List(props){
+    const [effect, setEffect] = useState('')
     return <>
-        <article className={styles.container}>
-            <figure className={styles.figure}>
+        <article 
+            className={styles.container} 
+            onMouseOver={()=> setEffect(styles.figure_over)} 
+            onMouseOut={()=> setEffect('')}
+        >
+            <figure className={`${styles.figure} ${effect}`}>
                 <img src={props.src}/>
             </figure>
             <p className={styles.text}>{props.name}</p>
