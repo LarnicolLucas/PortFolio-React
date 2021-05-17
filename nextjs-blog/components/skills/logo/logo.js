ea236ff0f59d00a9e20cff3f89517ee98db79645
Lucas <lucas.larnicol@gmail.com>
2021-05-17 18:20:49 +0200

    change animation on fond

--- nextjs-blog/components/skills/logo/logo.js
+++ nextjs-blog/components/skills/logo/logo.js
@@ -1,18 +1,13 @@
 import styles from './logo.module.sass'
-import {useRef, useEffect} from 'react'
+import {useRef, useEffect, useState} from 'react'
 import gsap from 'gsap'
 
-import Animate from '../animation.js'
+import Fond from './fond.js'
 
 export default function logo(props){
 
-    const fond = useRef();
     const svg = useRef();
-
-    const animFond = (bol)=> bol ? 
-        gsap.to(fond.current, {x: -25, duration: 1, ease: "expo.out"}) :
-        gsap.to(fond.current, {x: 0, duration: 1, ease: "expo.out"})
-    ;
+    const [trigFond, setTrigFond] = useState(false)
 
     const animSvg = (bol)=> bol ? 
         gsap.to(svg.current, {x: 25, duration: 1, ease: "expo.out"}) :
@@ -20,16 +15,13 @@ export default function logo(props){
     ;
 
     useEffect( ()=> {
-        animFond(props.trig);
         animSvg(props.trig);
     });
 
-    //<Animate dur="1s" path0={svgPath0} path1={svgPath1} target="fond0_0"/>
-
     return <>
-        <figure className={styles.container}>
+        <figure className={styles.container} onMouseOver={()=> setTrigFond(true)}>
             
-            <img src="images/skills/fond.svg" className={styles.svgs} ref={fond}/>
+            <Fond  trig={trigFond} id={props.id}/>
             <img src={props.src} className={styles.svgs} ref={svg}/>
             <img src="images/skills/fond.svg" className={styles.hide}/>
     
