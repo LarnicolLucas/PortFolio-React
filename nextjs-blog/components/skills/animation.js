9a6763421f1c73599f39122884b2e893010d653e
Lucas <lucas.larnicol@gmail.com>
2021-05-17 18:21:19 +0200

    add trig effect on animation morph

--- nextjs-blog/components/skills/animation.js
+++ nextjs-blog/components/skills/animation.js
@@ -1,15 +1,21 @@
+import {useRef, useEffect} from 'react'
+
 export default function AnimateSkill(props){
+
+    const anim = useRef();
+
+    useEffect(()=> props.trig ? anim.current.beginElement() : null)
+
     return <>
-        <svg style={{display: "none"}}>
-        <animate
+      <animate
+            ref={anim}
             href= {`#${props.target}`}
             attributeName= "d"
             from= {props.path0}
             to= {props.path1} 
             dur= {props.dur}
-            begin= "0s"
+            begin= "indefinite"
             fill= "freeze"
         />
-        </svg>
     </>
 }
