e2a8966c770e76067a9c38b17dd602ce04f19a82
Lucas <lucas.larnicol@gmail.com>
2021-05-20 09:59:35 +0200

    add responsivness to react component grid

--- nextjs-blog/components/utils/grid/grid.js
+++ nextjs-blog/components/utils/grid/grid.js
@@ -1,13 +1,20 @@
+import matchesQuery from '../matchesQuery/matches.js'
 import styles from './grid.module.sass'
 
+// props => addClass / nb / nb_responsive / responsive_size /
+
 export default function Grid(props){
+
+    const matches = matchesQuery(props.responsive_size)
+
     const classs = `${props.addClass} ${styles.grid}`
 
-    const gridPropreties = {
-        gridTemplateColumns: `repeat(${props.nb}, 1fr)`
-    }
+    const gridPropreties = (noResponsive) => noResponsive ? 
+        {gridTemplateColumns: `repeat(${props.nb}, 1fr)`} :
+        {gridTemplateColumns: `repeat(${props.nb_responsive}, 1fr)`}
+    
     return <>
-        <section className={classs} style={gridPropreties}>
+        <section className={classs} style={gridPropreties(matches)}>
             {props.children}
         </section>
     </>
