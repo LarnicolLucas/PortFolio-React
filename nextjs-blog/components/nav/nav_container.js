a1d8a354486987e36e08d03700940891fa28a549
Lucas <lucas.larnicol@gmail.com>
2021-05-22 10:26:31 +0200

    custom flex element

--- nextjs-blog/components/nav/nav_container.js
+++ nextjs-blog/components/nav/nav_container.js
@@ -1,29 +1,17 @@
-import styles from './nav_container.module.css'
-import Col from '../utils/col.js'
-import Row from '../utils/row.js'
+import styles from './nav_container.module.sass'
 
-import Link_nav from './nav_button.js'
+
 
 export default function nav_container(props){
     return <>
         <nav className={styles.nav_container}>
-            <Row reset={true}>
-                <Col size="8">
-                    <figure>  
-                        LOGO ICI
-                    </figure>
-                </Col>
-                <Col size="4">
-                    <Row reset={true}>
-                        <Col center={true} size="6">
-                            <Link_nav titre="About"></Link_nav>
-                        </Col>
-                        <Col center={true} size="6">
-                            <Link_nav titre="Contact"></Link_nav>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
+            <figure className={styles.figure}>
+                <img src="/images/logo.svg" className={styles.img}/>
+            </figure>
+            <section className={styles.link_container}>
+                <a>Contact</a>
+                <a>About</a>
+            </section>
         </nav>
     </>
 }
