f8ebec514fcb7fdc56d8bf52624e33507268cf63
Lucas <lucas.larnicol@gmail.com>
2021-05-22 09:38:22 +0200

    add opacity fade out

--- nextjs-blog/components/pres/animation_svg.js
+++ nextjs-blog/components/pres/animation_svg.js
@@ -4,8 +4,8 @@ const animation = (element, in_out) => {
 
     const randomCo = (min, max)=> Math.ceil((Math.random() * (max - min)) + min);
 
-    const anim_params_in = {x: 0, y: 0, duration: 1.5, ease: "elastic.out(1, 0.3)"};
-    const anim_params_out = {x: randomCo(700, -200), y: randomCo(500, 0) , duration: 1};
+    const anim_params_in = {opacity: 1, x: 0, y: 0, duration: 1.5, ease: "elastic.out(1, 0.3)"};
+    const anim_params_out = {opacity: 0, x: randomCo(700, -200), y: randomCo(500, 0) , duration: 1};
 
     if(in_out === true) return gsap.to(element, anim_params_in)
     return gsap.to(element, anim_params_out)
