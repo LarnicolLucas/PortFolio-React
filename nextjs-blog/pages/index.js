704deac85320e9a4fabd0a5fe97cd5a32e452623
Lucas <lucas.larnicol@gmail.com>
2021-05-22 10:26:51 +0200

    index is now handling mouse event

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,3 +1,5 @@
+import {useState} from 'react'
+
 import Layout from '../components/layout'
 import NAV_container from '../components/nav/nav_container.js'
 import SVG_intro_container from '../components/intro/svg_intro_container.js'
@@ -7,11 +9,18 @@ import Skills from '../components/skills/skills.js'
 
 export default function index(props){
 
+  const [mousePos, setMousePos] = useState([0, 0]);
+
+  const givePos = (e) => {
+    const convert = (value) => (value/50)
+    setMousePos([convert(e.clientX), convert(e.clientY)])
+}
+
   return <>
       <Layout>
-          <section className={styles.page}>
+          <section onMouseMove={givePos} className={styles.page}>
             <NAV_container/>
-            <SVG_intro_container/>
+            <SVG_intro_container pos={mousePos}/>
           </section>
           <Pres/>
           <Skills />
