ea79ecb292f3a7eee2c0fbb7210ef1bf49da2272
Lucas <lucas.larnicol@gmail.com>
2021-05-22 10:25:58 +0200

    event handler is deleguate to parent

--- nextjs-blog/components/intro/svg_intro_container.js
+++ nextjs-blog/components/intro/svg_intro_container.js
@@ -1,29 +1,17 @@
 import SVGIntro from './svg_intro.js'
-import React, {useState} from 'react'
 import styles from './svg_intro_container.module.sass'
 
 
-export default function containe_SVG() {
-
-    const [posX, setPosX] = useState(0);
-    const [posY, setPosY] = useState(0);
-
-    const givePos = (e) => {
-        const convert = (value) => (value/50)
-        const newPosX = convert(e.clientX);
-        const newPosY = convert(e.clientY);
-        setPosY(newPosY)
-        setPosX(newPosX)
-    }
+export default function containe_SVG(props) {
 
     return(
-        <section onMouseMove={givePos} className={styles.container_intro}>
+        <section className={styles.container_intro}>
             <figure className={styles.animeLogo}>
-                <SVGIntro src="/images/la.svg" posX={-2*posX}></SVGIntro>
-                <SVGIntro src="/images/left.svg" posX={posX} posY={posY}></SVGIntro>
-                <SVGIntro src="/images/right.svg" posX={-posX} posY={-posY}></SVGIntro>
-                <SVGIntro src="/images/slash.svg" posX={(-1.5) * posX} posY={1.7 * posY}></SVGIntro>
-                <SVGIntro src="/images/lu.svg" posX={0.8* posX}></SVGIntro>
+                <SVGIntro src="/images/la.svg" posX={-2*props.pos[0]}></SVGIntro>
+                <SVGIntro src="/images/left.svg" posX={props.pos[0]} posY={props.pos[1]}></SVGIntro>
+                <SVGIntro src="/images/right.svg" posX={-props.pos[0]} posY={-props.pos[1]}></SVGIntro>
+                <SVGIntro src="/images/slash.svg" posX={(-1.5) * props.pos[0]} posY={1.7 * props.pos[1]}></SVGIntro>
+                <SVGIntro src="/images/lu.svg" posX={0.8* props.pos[0]}></SVGIntro>
             </figure>
         </section>
     )
