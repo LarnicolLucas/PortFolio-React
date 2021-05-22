e0914bc113a7e3d14e3fd204db935043b1bfb93c
Lucas <lucas.larnicol@gmail.com>
2021-05-22 10:26:08 +0200

    uselles button

--- nextjs-blog/components/nav/nav_button.js
+++
@@ -1,9 +0,0 @@
-import styles from './nav_button.module.sass'
-
-export default function nav_button(props){
-    return <>
-        <div className={styles.nav_button} > 
-            <a>{props.titre}</a>
-        </div>
-    </>
-}
