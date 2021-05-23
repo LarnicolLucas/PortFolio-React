f93eda6780581865a4abcd7228c215a1982fd3f2
Lucas <lucas.larnicol@gmail.com>
2021-05-23 17:49:35 +0200

    syncronize name in intro

--- nextjs-blog/components/intro/svg_intro_container.js
+++ nextjs-blog/components/intro/svg_intro_container.js
@@ -7,11 +7,11 @@ export default function containe_SVG(props) {
     return(
         <section className={styles.container_intro}>
             <figure className={styles.animeLogo}>
-                <SVGIntro src="/images/la.svg" posX={-2*props.pos[0]}></SVGIntro>
+                <SVGIntro src="/images/la.svg" posX={2*props.pos[0]}></SVGIntro>
                 <SVGIntro src="/images/left.svg" posX={props.pos[0]} posY={props.pos[1]}></SVGIntro>
                 <SVGIntro src="/images/right.svg" posX={-props.pos[0]} posY={-props.pos[1]}></SVGIntro>
                 <SVGIntro src="/images/slash.svg" posX={(-1.5) * props.pos[0]} posY={1.7 * props.pos[1]}></SVGIntro>
-                <SVGIntro src="/images/lu.svg" posX={0.8* props.pos[0]}></SVGIntro>
+                <SVGIntro src="/images/lu.svg" posX={2* props.pos[0]}></SVGIntro>
             </figure>
         </section>
     )
