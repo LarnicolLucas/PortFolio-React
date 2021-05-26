df80f285fe5b4230197bb38ebe53aac480493117
Lucas <lucas.larnicol@gmail.com>
2021-05-26 17:20:24 +0200

    sideNav custom

--- nextjs-blog/components/dashboard/sidenav/sidenav.js
+++ nextjs-blog/components/dashboard/sidenav/sidenav.js
@@ -3,13 +3,16 @@ import styles from './sidenav.module.sass'
 export default function SideNav(prop){
 
         const list = [
-            {id:0, name: "Dashboard"},
-            {id:1, name: "History"},
-            {id:2, name: "Course"},
-            {id:3, name: "Items"}
+            {id:0, name: "Dashboard", src: "/images/dashboard/side/0.svg"},
+            {id:1, name: "History", src: "/images/dashboard/side/1.svg"},
+            {id:2, name: "Course", src: "/images/dashboard/side/2.svg"},
+            {id:3, name: "Items", src: "/images/dashboard/side/3.svg"}
         ];
 
-        const items = list.map(el => <p key={el.id}> {el.name} </p>)
+        const items = list.map(el => <section className={styles.containerList}>
+            <img className={styles.img} src={el.src} />
+            <p style={{paddingLeft: "2%"}} key={el.id}> {el.name} </p>
+        </section>)
     return <>
         <section className={styles.list_container}>
             {items}
