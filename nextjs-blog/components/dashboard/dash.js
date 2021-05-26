013cf277ad06c8e19546f42cacc5a290bb17a221
Lucas <lucas.larnicol@gmail.com>
2021-05-26 17:37:44 +0200

    fix glass morph on svg backgrond

--- nextjs-blog/components/dashboard/dash.js
+++ nextjs-blog/components/dashboard/dash.js
@@ -5,7 +5,12 @@ import Content from './content/content.js'
 
 export default function Dash(prop){
     return <>
-        <section className={`${styles.container} ${glass.glass}`}>
+        <section className={`${styles.container} `}>
+            <img style={{ top: "-10%", left: "2%" }} className={styles.img} src="/images/dashboard/fond/0.svg" />
+            <img style={{ top: "15%", left: "80%" }} className={styles.img} src="/images/dashboard/fond/1.svg" />
+            <img style={{ top: "-20%", left: "35%" }} className={styles.img} src="/images/dashboard/fond/2.svg" />
+            <img style={{ top: "70%", left: "50%" }} className={styles.img} src="/images/dashboard/fond/0.svg" />
+            <section className={`${styles.glassMorphFond} ${glass.glass}`}></section>
             <aside className={styles.sideNav}>
                 <Sidenav />
             </aside>
--- nextjs-blog/components/dashboard/dashboard.js
+++ nextjs-blog/components/dashboard/dashboard.js
@@ -5,9 +5,6 @@ import styles from './dashboard.module.sass'
 export default function Dashnoard(props){
     return <>
         <main className={styles.container}>
-            <img style={{ top: "3%", left: "2%" }} className={styles.img} src="/images/dashboard/fond/0.svg" />
-            <img style={{ top: "30%", left: "66%" }} className={styles.img} src="/images/dashboard/fond/1.svg" />
-            <img style={{ top: "0%", left: "35%" }} className={styles.img} src="/images/dashboard/fond/2.svg" />
             <Dash />
         </main>
     </>
