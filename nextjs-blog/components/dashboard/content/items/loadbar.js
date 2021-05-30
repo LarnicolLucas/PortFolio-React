6132ead4ee430701bd8bbd26528ca5f9fecf8540
Lucas <lucas.larnicol@gmail.com>
2021-05-30 12:39:34 +0200

    add load effect on items dashboard

--- nextjs-blog/components/dashboard/content/items/loadbar.js
+++ nextjs-blog/components/dashboard/content/items/loadbar.js
@@ -1,21 +1,31 @@
 import styles from './items.module.sass'
+import {useEffect, useRef} from 'react'
+import gsap from 'gsap'
 
 export default function load(props){
+
+    const loadBarDom = useRef();
 
     const style = {
         width: "50%",
         height: "7px",
         boxShadow: "2px 2px 2px #515151"
-    }
+    };
     const styleBar = {
-        width: `${props.value}%`,
+        width: `0%`,
         height: "100%",
         background: props.color
-    }
+    };
+
+    useEffect(()=> {
+        gsap.to(loadBarDom.current, {width: `${props.value}%`, duration: 2, ease: "elastic.out( 1, 0.3)",})
+    }, [])
+
+
     return <>
         <article className={styles.containerLoad} style={style}>
 
-            <div className={styles.containerLoad} style={styleBar}></div>
+            <div className={styles.containerLoad} style={styleBar} ref={loadBarDom}></div>
 
         </article>
     </>
