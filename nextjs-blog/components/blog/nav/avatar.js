aee65c8f56be7e2264c7764450445fca4c98915e
Lucas <lucas.larnicol@gmail.com>
2021-06-03 17:47:50 +0200

    avatar for blog

---
+++ nextjs-blog/components/blog/nav/avatar.js
@@ -0,0 +1,12 @@
+import styles from './avatar.module.sass'
+
+export default function Avatar(){
+    return <>
+        <section className={styles.container_}>
+            <figure className={styles.image+" "+styles["is-64x64"]+" "+styles.reset}>
+                <img className={styles["is-rounded"]} src="/images/face/total.svg" />
+            </figure>
+        </section>
+
+    </>
+}
