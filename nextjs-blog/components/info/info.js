d26dca3c1ba80abfee8fcd5751600cfc42ae393e
Lucas <lucas.larnicol@gmail.com>
2021-06-03 15:14:46 +0200

    better UI on info

--- nextjs-blog/components/info/info.js
+++ nextjs-blog/components/info/info.js
@@ -3,7 +3,7 @@ import styles from './info.module.sass'
 export default function Info(props){
     return <>
         <article className={styles.parent}>
-            <div className={styles.button}>INFO</div>
+            <div className={styles.button}>About</div>
             <section className={styles.container}>
 
                 <h3>{props.text.intro}</h3>
