aaedc41503622fd0fccd81794367d070a9ca203d
Lucas <lucas.larnicol@gmail.com>
2021-06-04 20:10:19 +0200

    nav Items components blog

--- nextjs-blog/components/blog/main/sideNav/navItem/navItem.js
+++ nextjs-blog/components/blog/main/sideNav/navItem/navItem.js
@@ -2,7 +2,22 @@ import styles from './navItem.module.sass'
 
 export default function NavItem(props){
 
+    const styleSelected= {
+        background: props.color
+    }
+
     return <>
-        <p> {props.data.name} </p>
+        <article className={styles.container_}>
+
+            <section className={styles.containerImgText}>
+                <figure className={styles.figure_} style={styleSelected}>
+                    <img className={styles.img_} src={"/images/blog/icons/"+props.data.id+".svg"} />
+                </figure>
+                <h5>{props.data.name}</h5>
+            </section>
+
+            <div className={styles.divRounded} style={styleSelected}></div>
+
+        </article>
     </>
 }
