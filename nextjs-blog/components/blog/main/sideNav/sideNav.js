cd5b738630cef12f4a10f4777962ac11b1678905
Lucas <lucas.larnicol@gmail.com>
2021-06-04 20:47:11 +0200

    add nav item send button

--- nextjs-blog/components/blog/main/sideNav/sideNav.js
+++ nextjs-blog/components/blog/main/sideNav/sideNav.js
@@ -1,18 +1,25 @@
 import styles from './sideNav.module.sass'
 import NavItem from './navItem/navItem.js'
+import Box from './box/box.js'
 
-export default function SideBar(){
+export default function SideBar(props){
 
     const list = [
         {id: 0, name: 'Home'},
         {id: 1, name: 'LifeStyle'},
         {id: 2, name: 'CodeDiary'},
-    ].map(el => <NavItem key={el.id} data={el}/>)
+    ].map(el => <NavItem key={el.id} data={el} color={props.color}/>)
 
     return <>
         <aside className={styles.container_}>
 
-            {list}
+            <section className={styles.containerItem}>
+
+                {list}
+
+            </section>
+
+            <Box color={props.color}/>
 
         </aside>
     </>
