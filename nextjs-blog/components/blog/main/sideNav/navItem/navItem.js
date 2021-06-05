b8bdd8006908118583c2acb46f638c788ab2a3c3
Lucas <lucas.larnicol@gmail.com>
2021-06-05 15:16:48 +0200

    color style in sidenav

--- nextjs-blog/components/blog/main/sideNav/navItem/navItem.js
+++ nextjs-blog/components/blog/main/sideNav/navItem/navItem.js
@@ -1,4 +1,5 @@
 import styles from './navItem.module.sass'
+
 import {useState} from 'react'
 
 export default function NavItem(props){
@@ -6,19 +7,23 @@ export default function NavItem(props){
 
     const styleSelected= {
         background: props.clicked ? props.color : "#eee"
-    }
+    };
     const styleSelectedDiv= {
         background: props.clicked ? props.color : "none"
-    }
+    };
+    const stylePara= {
+        color: props.clicked ? "#000" : "#81878B"
+    };
+    const svg = props.clicked ? "/images/blog/icons/"+props.data.id+"W.svg" : "/images/blog/icons/"+props.data.id+".svg"
 
     return <>
         <article className={styles.container_} onClick={()=> props.fnClicked(props.id)}>
 
             <section className={styles.containerImgText}>
                 <figure className={styles.figure_} style={styleSelected}>
-                    <img className={styles.img_} src={"/images/blog/icons/"+props.data.id+".svg"} />
+                    <img className={styles.img_} src={svg} />
                 </figure>
-                <h5>{props.data.name}</h5>
+                <h6 style={stylePara}>{props.data.name}</h6>
             </section>
 
             <div className={styles.divRounded} style={styleSelectedDiv}></div>
