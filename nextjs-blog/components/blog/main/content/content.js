032ca3515660b21c50a2dd5cc5c3140f4a49829d
Lucas <lucas.larnicol@gmail.com>
2021-06-06 21:29:31 +0200

    get element on change

--- nextjs-blog/components/blog/main/content/content.js
+++ nextjs-blog/components/blog/main/content/content.js
@@ -1,31 +1,22 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import styles from './content.module.sass'
 import Article from './article/article.js'
 
 import ReadArticle from './readArticle/readArticle.js'
 
 import NewArticle from './newArticle/newArticle.js'
+import ApiCallGet from '../../apiCallGet'
 
 export default function Content(props){
 
-    //props.tagFilter , props.newArticle , readArticle
+    
 
     const [readPage, setReadPage] = useState(0);
     const readArticle = (id) => {
         props.changeFilter(false, 0);
         setReadPage(id)}
 
-    const articleDatas = [{
-        id: 0,
-        _id: "B0X4f4g6",
-        src: "/images/blog/articles/0.jpg",
-        tag: "codeDiary",
-        title: "The day where i code my own BLOG... And everything goes fine",
-        date: "10/12/2012",
-        name: "Lucas Larnicol",
-        img: "/images/face/total.svg",
-        content: "Contenu ici. Lorem BLABLABAL"      
-    }];
+    const [articleDatas, setArticleDatas] = useState([]);
 
     const mapListArticle = (list) => list.map(el=> <Article key={el.id} data={el} readArticle={readArticle}/>);
 
@@ -39,6 +30,18 @@ export default function Content(props){
             articles :
             <ReadArticle color={props.color} data={articleDatas.filter(el => el._id === readPage)[0]} />
     ;
+
+    useEffect(async ()=> {
+        try {
+            const res = await ApiCallGet("allArticles");
+
+            setArticleDatas(res);
+
+        }catch(err){
+            console.log(err)
+        }
+    });
+
     return <>
         <section className={styles.container_}>
             {test}
