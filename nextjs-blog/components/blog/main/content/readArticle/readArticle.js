84997e848ec79402a95cfb5b8729861dc0218c0c
Lucas <lucas.larnicol@gmail.com>
2021-06-06 10:07:56 +0200

    add component read Article

---
+++ nextjs-blog/components/blog/main/content/readArticle/readArticle.js
@@ -0,0 +1,22 @@
+import styles from './readArticle.module.sass'
+import Author from '../article/author.js'
+
+export default function Read(props){
+
+    return <>
+        <article className={styles.container_}>
+
+        
+            <h1 className={styles.title_}> {props.data.title} </h1>
+
+            <figure className={styles.figure_}>
+                <img className={styles.img_} src={props.data.src} />
+            </figure>
+            <Author author={props.data.author} date={props.data.date}/>
+            
+            <p>{props.data.content}</p>
+            
+
+        </article>
+    </>
+}
