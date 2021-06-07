654c6333b264585bb42d4c5cade868468e46c00d
Lucas <lucas.larnicol@gmail.com>
2021-06-07 21:07:52 +0200

    add loader and use Effect on props.tag change BLOG

--- nextjs-blog/components/blog/main/content/content.js
+++ nextjs-blog/components/blog/main/content/content.js
@@ -7,9 +7,9 @@ import ReadArticle from './readArticle/readArticle.js'
 import NewArticle from './newArticle/newArticle.js'
 import ApiCallGet from '../../apiCallGet'
 
-export default function Content(props){
+import Loader from '../../../utils/loader/loader.js'
 
-    
+export default function Content(props){
 
     const [readPage, setReadPage] = useState(0);
     const readArticle = (id) => {
@@ -18,7 +18,7 @@ export default function Content(props){
 
     const [articleDatas, setArticleDatas] = useState([]);
 
-    const mapListArticle = (list) => list.map(el=> <Article key={el.id} data={el} readArticle={readArticle}/>);
+    const mapListArticle = (list) => list.map(el=> <Article key={el._id} data={el} readArticle={readArticle}/>);
 
     const articles = props.tagFilter.tag != "" ?
         mapListArticle(articleDatas.filter(el=> el.tag === props.tagFilter.tag)) :
@@ -31,19 +31,24 @@ export default function Content(props){
             <ReadArticle color={props.color} data={articleDatas.filter(el => el._id === readPage)[0]} />
     ;
 
+    const [loader, setLoader] = useState(<Loader />)
+
     useEffect(async ()=> {
         try {
+            setArticleDatas([]);
+            setLoader(<Loader />);
             const res = await ApiCallGet("allArticles");
-
+            setLoader("");
             setArticleDatas(res);
 
         }catch(err){
             console.log(err)
         }
-    });
+    }, [props.tagFilter]);
 
     return <>
         <section className={styles.container_}>
+            {loader}
             {test}
         </section>
     </>
