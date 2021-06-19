87d8b4d263875806d2ea30829f06a55a3557be4c
Lucas <lucas.larnicol@gmail.com>
2021-06-19 21:15:08 +0200

    newLogin Process

--- nextjs-blog/pages/api/login/newLogin.js
+++ nextjs-blog/pages/api/login/newLogin.js
@@ -1,6 +1,7 @@
 import createSalt from './components/createSalt'
 import hashSalt from './components/hashSlat';
 import checkNewUser from './components/checkNewUser/checkNewUser';
+import post from './components/post';
 
 const params = {
     db: {
@@ -12,16 +13,25 @@ const params = {
 
 export default async function HandleRequest(req, res){
 
+    const ip = req.socket.remoteAddress || req.headers['x-forwarded-for'];
     const body = req.body;
 
     try{
-        const check = await checkNewUser(body.login, body.password, 123, params.db, params.regex);
+        const check = await checkNewUser(body.login, body.password, ip, params.db, params.regex);
 
-        if(check.error) return res.status(401).json({ error: check.message });
+        if(check.error) return res.status(401).json(check);
 
         const newSalt = createSalt();
+        const hashedPassword = hashSalt(body.password, newSalt);
 
-        //hashSalt(body.password, newSalt)
+        const record = await post({
+            login: body.login, 
+            password: hashedPassword,
+            salt: newSalt,
+            ip: ip
+        }, params.db);
+
+        console.log(record)
 
     }catch(err){
         console.log(err)
