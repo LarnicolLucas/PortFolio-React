055fcd5ca3cba09a1496650f1bc4f165544b58d2
Lucas <lucas.larnicol@gmail.com>
2021-06-20 20:56:51 +0200

    add login api

--- nextjs-blog/pages/api/login/login.js
+++ nextjs-blog/pages/api/login/login.js
@@ -1,28 +1,31 @@
-import connectMongo from '../dbConnect.js'
-
-export default function HandleRequest(req, res){
-
-    const body = req.body
-
-    console.log(body)
-
-    /*const query = async (client)=> {
-        try{
-        
-            const db = client.db('blog');
-        
-            const col = db.collection("article");        
-            const result = await col.insertOne(Object.assign({}, body, {date: new Date()}));
-    
-            return res.json({data: result})
-    
-        }catch(err){
-            console.log(err)
-        }
-    }*/
-
-    //return connectMongo(query)
+import BadAttempt from './components/addBadAttempt.js';
+import Get from './components/get.js'
+import hashSalt from './components/hashSlat.js';
+import params from "./params"
+import wrongAutent from "./components/wrongAutent"
 
 
+export default async function HandleRequest(req, res){
 
+    const body = req.body;
+    const {ObjectID} = require("mongodb");
+
+    try {
+
+        const user = await Get({login: body.login}, params.db);
+        const userId = new ObjectID(user[0]._id);
+
+        //user don't exist
+        if(user.length === 0) return wrongAutent(res, "Login or Password is not register");
+        //user has blocked count
+        if((new Date()) - user[0].blockTime < params.resetAttemptTime*60*60*1000) return wrongAutent(res, "Count has been blocked. Try Later")
+
+        const comparePasswords = hashSalt(body.password, user[0].salt) === user[0].password;
+
+        if(!comparePasswords) return BadAttempt(userId, res, params.db, user[0].attempt);
+
+
+    }catch(err){
+        console.log(err)
+    }
 }
