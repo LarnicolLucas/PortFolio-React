3fa718b98b5a76241d3d0152f37ddd17b44d3493
Lucas <lucas.larnicol@gmail.com>
2021-06-20 20:55:48 +0200

    add attempt connection

---
+++ nextjs-blog/pages/api/login/components/addBadAttempt.js
@@ -0,0 +1,25 @@
+import Update from "./update"
+import wrongAutent from "./wrongAutent"
+import BlockCount from './addBlockCount.js';
+
+export default async function BadAttempt(id, res, dbParams, userAttempt) {
+
+    if(userAttempt >= 5) return BlockCount(id, res, dbParams);
+
+    try{
+
+        const filter = { _id: id };
+        const update = {
+            $inc: {
+                attempt: 1,
+            },
+        };
+
+        const record = await Update(filter, update, dbParams)
+
+        return wrongAutent(res, "Count has been blocked. Try Later")
+
+    }catch(err){
+        console.log(err)
+    }
+}
