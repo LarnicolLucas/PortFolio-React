89b94b8b3904f83c74d842599f46b50879b96eab
Lucas <lucas.larnicol@gmail.com>
2021-06-21 20:47:07 +0200

    add reset attempt

--- nextjs-blog/pages/api/login/login.js
+++ nextjs-blog/pages/api/login/login.js
@@ -3,6 +3,7 @@ import Get from './components/get.js'
 import hashSalt from './components/hashSlat.js';
 import params from "./params"
 import wrongAutent from "./components/wrongAutent"
+import ResetAttempt from './components/resetAttempt.js';
 
 
 export default async function HandleRequest(req, res){
@@ -18,11 +19,16 @@ export default async function HandleRequest(req, res){
         //user don't exist
         if(user.length === 0) return wrongAutent(res, "Login or Password is not register");
         //user has blocked count
+
         if((new Date()) - user[0].blockTime < params.resetAttemptTime*60*60*1000) return wrongAutent(res, "Count has been blocked. Try Later")
 
         const comparePasswords = hashSalt(body.password, user[0].salt) === user[0].password;
 
         if(!comparePasswords) return BadAttempt(userId, res, params.db, user[0].attempt);
+
+        const ClearAttempt = await ResetAttempt(userId, params.db);
+
+        return res.status(200).json({error: false, message: "User autentified"})
 
 
     }catch(err){
