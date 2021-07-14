d9cf84c2b6eb4774fa168550f6b093f62674224a
Lucas <lucas.larnicol@gmail.com>
2021-07-14 09:31:50 +0200

    updtate Card

--- nextjs-blog/components/portfolio/card.js
+++ nextjs-blog/components/portfolio/card.js
@@ -3,22 +3,18 @@ import Link from 'next/link'
 
 export default function Card(props){
     return <>
-        <section className={styles.card_container}>
-
-            <Link href={props.link}>
-
-                <article className={styles.card}>
-                    <figure className={styles.figure}>
-                            <img className={styles.img} src={props.src}/>
-                        
-                    </figure>
-                    <figcaption className={styles.figcaption}>
-                        <p>{props.description}</p>
-                    </figcaption>
-                </article>
-
-            </Link>
-
-        </section>
+        <Link href={props.link} passHref>
+        <article className={styles.card}>
+            
+            <figure className={styles.figure}>
+                
+                <img className={styles.img} src={props.src}/>
+                 
+            </figure>
+            <figcaption className={styles.figcaption}>
+                <p>{props.description}</p>
+            </figcaption> 
+        </article>
+        </Link>
     </>
 }
